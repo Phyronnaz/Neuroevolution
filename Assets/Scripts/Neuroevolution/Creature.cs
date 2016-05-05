0186ff81357d90a1d159884b32ac8292c5f4f244
Phyronnaz <phyronnaz@gmail.com>
2016-05-05 10:03:59 +0200

    Box2D V1

---
+++ Assets/Scripts/Neuroevolution/Creature.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using FarseerPhysics.Dynamics.Joints;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using Microsoft.Xna.Framework;
+
+namespace Assets.Scripts.Neuroevolution
+{
+    class Creature
+    {
+        World world;
+        List<RevoluteJoint> revoluteJoints;
+        public Creature (List<FVector2> positions)
+        {
+            world = new World(new FVector2(0, -9.8f));
+            revoluteJoints = new List<RevoluteJoint>();
+            foreach(var p in positions)
+            {
+                world.AddBody(BodyFactory.CreateBody(world, p));
+            }
+        }
+
+        public void Update (float dt)
+        {
+            world.Step(dt);
+            revoluteJoints[0].
+        }
+
+        public void AddDistanceJoint (int a, int b)
+        {
+            world.AddJoint(JointFactory.CreateDistanceJoint(world, world.BodyList[a], world.BodyList[b], FVector2.Zero, FVector2.Zero));
+        }
+
+        public void AddRevoluteJoint(int a, int b, int anchor, float lowerLimit, float upperLimit)
+        {
+            var j = JointFactory.CreateRevoluteJoint(world.BodyList[a], world.BodyList[b], world.BodyList[anchor].Position);
+            j.SetLimits(lowerLimit, upperLimit);
+            world.AddJoint(j);
+            revoluteJoints.Add(j);
+        }
+
+        public List<Body> GetBodies ()
+        {
+            return world.BodyList;
+        }
+
+        public List<FarseerJoint> GetJoints ()
+        {
+            return world.JointList;
+        }
+    }
+}
---
+++ Assets/Scripts/Neuroevolution/CreatureRenderer.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using FarseerPhysics.Dynamics.Joints;
+using FarseerPhysics.Dynamics;
+
+
+namespace Assets.Scripts.Neuroevolution
+{
+    class CreatureRenderer : MonoBehaviour
+    {
+        List<Transform> nodes;
+        List<LineRenderer> lines;
+        Color color = Random.ColorHSV();
+
+        void Update(List<Body> bodies, List<FarseerJoint> joints)
+        {
+            //Select distances joints
+            var distanceJoints = new List<DistanceJoint>();
+            foreach (var j in joints)
+            {
+                if (j is DistanceJoint)
+                {
+                    distanceJoints.Add((DistanceJoint)j);
+                }
+            }
+
+            //Update lines
+            if (lines == null || lines.Count != distanceJoints.Count)
+            {
+                if (lines != null)
+                {
+                    foreach (var l in lines)
+                    {
+                        Destroy(l);
+                    }
+                }
+                lines = new List<LineRenderer>();
+                foreach (var j in distanceJoints)
+                {
+                    var l = new LineRenderer();
+                    var material = new Material(Shader.Find("Diffuse"));
+                    l.material = material;
+                    l.SetColors(color, color);
+                    lines.Add(l);
+                }
+            }
+            for(var i = 0; i < lines.Count; i ++)
+            {
+                lines[i].SetPosition(0, new Vector3(distanceJoints[i].BodyA.Position.X, distanceJoints[i].BodyA.Position.Y, 0));
+                lines[i].SetPosition(1, new Vector3(distanceJoints[i].BodyB.Position.X, distanceJoints[i].BodyB.Position.Y, 0));
+            }
+
+            //Update nodes
+            if (nodes == null || nodes.Count != bodies.Count)
+            {
+                nodes = new List<Transform>();
+                foreach (var b in bodies)
+                {
+                    var go = Instantiate(Resources.Load("Circle"), new Vector2(b.Position.X, b.Position.Y), Quaternion.identity) as GameObject;
+                    go.GetComponent<SpriteRenderer>().color = new Color(1.0f - color.r, 1.0f - color.g, 1.0f - color.b);
+                    nodes.Add(go.transform);
+                }
+            }
+            else
+            {
+                for (var i = 0; i < nodes.Count; i++)
+                {
+                    nodes[i].position = new Vector2(bodies[i].Position.X, bodies[i].Position.Y);
+                }
+            }
+        }
+    }
+}
